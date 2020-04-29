e0fcb432903d8b44aa47793377fcc0da679aafdd
Mostafa Abuelnoor <mostafasaher@gmail.com>
2020-04-30 00:34:59 +0400

    Fully implemented appointment showing with overbooking in mind

--- routes/appointments.js
+++ routes/appointments.js
@@ -1,97 +1,116 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const moment = require('moment');
-
-
+const moment = require("moment");
 
 //Appointment model
-const Appointment = require('../models/Appointment');
+const Appointment = require("../models/Appointment");
 
 //Doctors page
-router.get('/docs', (req, res) => res.render('docs'));
+router.get("/docs", (req, res) => res.render("docs"));
 
 //Appointments page
-router.get('/appts', (req, res) => res.render('appts'));
+router.get("/appts", (req, res) => res.render("appts"));
 
 //schedules page
-router.get('/schedule', (req, res) => res.render('schedule', {doc: docs}));
-
+router.get("/schedule", (req, res) =>
+  res.render("schedule", {
+    doc: docs,
+    dat: dates,
+    spec: specializations,
+    pat: patients,
+    avail: available,
+  })
+);
 
 //Doctors handle
-router.post('/docs', (req, res) => {
-    const { doctor, specialization, date1 } = req.body;
+router.post("/docs", (req, res) => {
+  const { doctor, specialization, date1 } = req.body;
 
-    let errors = [];
-    var date = moment(date1);
-    var date = date.format("DD-MM-YYYY HH:mm"); //Thank you god for moment.js
+  let errors = [];
+  var date = moment(date1);
+  var date = date.format("DD-MM-YYYY HH:mm"); //Thank you god for moment.js
 
-    //check required fields
-    if (!doctor || !specialization || !date) {
-        errors.push({ msg: 'Please fill in all the fields!' });
-    }
+  //check required fields
+  if (!doctor || !specialization || !date) {
+    errors.push({ msg: "Please fill in all the fields!" });
+  }
 
-    if (errors.length > 0) {
-        res.render('docs', {
-            errors,
-            doctor,
-            specialization,
-            date
+  if (errors.length > 0) {
+    res.render("docs", {
+      errors,
+      doctor,
+      specialization,
+      date,
+    });
+  } else {
+    //all fields filled
+    Appointment.findOne({ doctor: doctor, date: date }).then((appointment) => {
+      if (appointment) {
+        errors.push({ msg: "Slot is already added" });
+        res.render("docs", {
+          errors,
+          doctor,
+          specialization,
+          date,
         });
-    }
-    else {
-        //all fields filled
-        Appointment.findOne({ doctor: doctor, date: date })
-            .then(appointment => {
-                if (appointment) {
-                    errors.push({ msg: 'Slot is already added' });
-                    res.render('docs', {
-                        errors,
-                        doctor,
-                        specialization,
-                        date
-                    });
-                } else {
-                    const newApp = new Appointment({
-                        doctor,
-                        specialization,
-                        date
-                    });
+      } else {
+        const newApp = new Appointment({
+          doctor,
+          specialization,
+          date,
+        });
 
-                    newApp.save()
-                        .then(appointment => {
-                            req.flash('success_msg', 'Doctor added!');
-                            res.redirect('/appointments/appts');
-                        })
-                        .catch(err => console.log(err));
-                }
-            })
-    }
+        newApp
+          .save()
+          .then((appointment) => {
+            req.flash("success_msg", "Doctor added!");
+            res.redirect("/appointments/appts");
+          })
+          .catch((err) => console.log(err));
+      }
+    });
+  }
 });
+
 var docs = [];
-router.post('/appts', (req, res) => {
-    
-    const { doctor } = req.body;
+var dates = [];
+var specializations = [];
+var patients = [];
+var available = [];
 
-    function getApp(doctor) {
-        var query = Appointment.find({ doctor: doctor });
-        return query;
+router.post("/appts", (req, res) => {
+  let errors = [];
+  const { doctor } = req.body;
+  var notEmpty = false; //flag
+  function getApp(doctor) {
+    var query = Appointment.find({ doctor: doctor });
+    return query;
+  }
+
+  var query = getApp(doctor);
+  query.exec(function (err, appts) {
+    if (err) return console.log(err);
+    for (let i = 0; i < appts.length; i++) {
+      docs[i] = appts[i].doctor;
+      dates[i] = appts[i].date;
+      specializations[i] = appts[i].specialization;
+      available[i] = appts[i].available;
+      patients[i] = appts[i].patients;
+
+      if (Object.values(patients[i]).length > 0) {
+        notEmpty = true;
+      }
     }
-
-    var query = getApp(doctor);
-    query.exec(function (err, appts) {
-        if (err)
-            return console.log(err);
-        for (let i = 0; i < appts.length; i++) {
-            docs[i] = Object.values(appts[i]);
-            console.log(docs[i]);
-        }
-        res.redirect('/appointments/schedule');
-    });    
-//    console.log(docs[0]);
-
+    if (notEmpty) {
+      req.flash("success_msg", "Appointments found!");
+      res.redirect("/appointments/schedule");
+    } else {
+      errors.push({ msg: "No appointments booked for " + docs[0] });
+      res.render("appts", {
+        errors,
+      });
+    }
+  });
 });
-
-
-
 
 module.exports = router;
